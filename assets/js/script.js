8626242dd8ba6adeea8d400ebb70fce4bb68288a
Miguel Mulet <mmiguel032202@gmail.com>
2022-02-06 18:20:59 -0500

    Added initPage Hour function + Background functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,65 @@ var sevenPm = $("#19pm");
 var hour = moment().hours();
 var userInput;
 var hourSpan;
+
+// Date + Hour per schedule
+var interval = setInterval(function() {
+    var momentNow = moment();
+    $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                        + momentNow.format('dddd')
+                         .substring(0,3).toUpperCase());
+    $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+}, 100);
+  
+function initPage() {
+    console.log("Current Hour " + hour);
+
+    var init9 = JSON.parse(localStorage.getItem("09:00 AM"));
+    nineAm.val(init9);
+  
+    var init10 = JSON.parse(localStorage.getItem("10:00 AM"))
+    tenAm.val(init10);
+    
+    var init11 = JSON.parse(localStorage.getItem("11:00 AM"))
+    elevenAm.val(init11);
+    
+    var init12 = JSON.parse(localStorage.getItem("12:00 PM"))
+    twelvePm.val(init12);
+    
+    var init1 = JSON.parse(localStorage.getItem("01:00 PM"))
+    onePm.val(init1);
+    
+    var init2 = JSON.parse(localStorage.getItem("02:00 PM"))
+    twoPm.val(init2);
+    
+    var init3 = JSON.parse(localStorage.getItem("03:00 PM"))
+    threePm.val(init3);
+   
+    var init4 = JSON.parse(localStorage.getItem("04:00 PM"))
+    fourPm.val(init4);
+    
+    var init5 = JSON.parse(localStorage.getItem("05:00 PM"))
+    fivePm.val(init5);
+    
+    var init6 = JSON.parse(localStorage.getItem("06:00 PM"))
+    sixPm.val(init6);
+    
+    var init7 = JSON.parse(localStorage.getItem("07:00 PM"))
+    sevenPm.val(init7);
+}
+
+function background () {
+    $(".form-control").each(function () {
+        var timeTest = parseInt($(this).attr("id"));
+        hour = parseInt(hour);
+        console.log(timeTest);
+        console.log(hour);
+        if (hour > timeTest) {
+            $(this).addClass("past");
+        } else if (hour < timeTest) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+    });
+}
